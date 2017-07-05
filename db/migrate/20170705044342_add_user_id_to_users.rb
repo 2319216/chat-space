9826245db39d67836a4fbfb00e2077186c9f4843
2319216 <matsufujimasahito@matsufujimasahitonoMacBook-Pro.local>
2017-07-05 18:52:36 +0900

    edit_sign-up

--- db/migrate/20170705044342_add_user_id_to_users.rb
+++ db/migrate/20170705044342_add_user_id_to_users.rb
@@ -1,6 +1,5 @@
 class AddUserIdToUsers < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :name, :string
-    add_column :users, :user_id, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 20170705044342) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
-    t.integer  "user_id"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
