f6dd005fe10470e4c515747e41717ab5793e76ee
2319216 <matsufujimasahito@matsufujimasahitonoMacBook-Pro.local>
2017-07-05 19:05:04 +0900

    edit_sign-up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :name, presence: true
+  validates :name, :email, presence: true
   validates :password, presence: true, length: { minimum: 8 }
 end
