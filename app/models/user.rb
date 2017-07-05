330b74446a5377e47ef744086ce8af2458779cf1
2319216 <matsufujimasahito@matsufujimasahitonoMacBook-Pro.local>
2017-07-05 20:33:49 +0900

    Information-edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :name, :email, presence: true
-<<<<<<< Updated upstream
   validates :password, presence: true, length: { minimum: 8 }
-=======
-  varidates :password, presence: true, length: { minimum: 8 }
->>>>>>> Stashed changes
 end
--- db/migrate/20170705044342_add_user_id_to_users.rb
+++ db/migrate/20170705044342_add_user_id_to_users.rb
@@ -1,9 +1,6 @@
 class AddUserIdToUsers < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :name, :string
-<<<<<<< Updated upstream
-=======
-    add_column :users, :user_id, :integer
->>>>>>> Stashed changes
+
   end
 end
