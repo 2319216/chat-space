f301981f9c911ef57f23d99a854b61346791abb0
2319216 <matsufujimasahito@matsufujimasahitonoMacBook-Pro.local>
2017-07-14 15:50:46 +0900

    controller_test

---
+++ spec/controllers/messages_controller_spec.rb
@@ -0,0 +1,77 @@
+require 'rails_helper'
+
+describe MessagesController, type: :controller do
+  let(:user) { create(:user) }
+  let(:group) { create(:group) }
+  let(:message) { build(:message) }
+
+  describe 'GET #index' do
+    context 'ログインしている場合' do
+      before do
+        login_user user
+      end
+
+      it "assigns the requested message to @messages" do
+        get :index, params: { group_id: group }
+        expect(assigns(:message)).to be_a_new(Message)
+      end
+
+      it "assigns the requested message to @messages" do
+        messages = group.messages
+        get :index, params: { group_id: group }
+        expect(assigns(:messages)).to match(messages.sort{ |a, b| b.created_at <=> a.created_at } )
+      end
+
+      it "assigns the requested message to @messages" do
+        get :index, params: { group_id: group }
+        expect(assigns(:group)).to eq group
+      end
+
+      it "renders the :index template" do
+        get :index, params: { group_id: group }
+        expect(response).to render_template :index
+      end
+    end
+
+    context 'ログインしていない場合' do
+      it "redirect_to new_user_session_path" do
+        get :index, params: { group_id: group }
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+  end
+
+  describe 'POST #create' do
+    context 'ログインしている場合' do
+      before do
+        login_user user
+      end
+
+      it "saves the new record in the DB" do
+        expect{post :create, params:{group_id: group, message: attributes_for(:message)}}.to change(Message, :count).by(1)
+      end
+
+      it "not to saves the new record in the DB" do
+        message = attributes_for(:message, message: nil, image: nil)
+        expect{ post :create, params: { group_id: group, message: message }}.not_to change(Message, :count)
+      end
+
+      it "message save redirect_to group_messages_path" do
+        post :create, params: { group_id: group, message: attributes_for(:message) }
+        expect(response).to redirect_to group_messages_path
+      end
+
+      it "not to save renders the :index template" do
+        get :index, params: { group_id: group }
+        expect(response).to render_template :index
+      end
+
+    context 'ログインしていない場合' do
+      it "renders the :index template" do
+        get :index, params: { group_id: group }
+        expect(response).to render_template :index
+      end
+    end
+    end
+  end
+end
--- spec/factories/message.rb
+++ spec/factories/message.rb
@@ -1,11 +1,10 @@
 FactoryGirl.define do
 
   factory :message do
-    message Faker::Name.name
+    message               {Faker::Name.name}
     user_id               "1"
     group_id              "1"
     image File.open(File.join(Rails.root, '/spec/fixtures/Manchester-city.jpg'))
 
   end
 end
-
